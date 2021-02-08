23449f7cafef5e65c1a6e6247e7768adf871cd08
Linuxviz <Linuxviz@yandex.ru>
2021-02-08 18:21:54 +0700

    patch newsbycategory template reverse from main page

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView, NewsDetailView, NewsByCategoryView, NewsByTagVi
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
-    path('news/<int:pk>/', NewsDetailView.as_view(), name='news_detail_page'),
     path('newsByCategory/<str:category_name>/', NewsByCategoryView.as_view(), name='news_by_category'),
     path('newsByTag/<str:tag_name>/', NewsByTagView.as_view(), name='news_by_tag'),
+    path('news/<int:pk>/', NewsDetailView.as_view(), name='news_detail_page'),
 ]
--- web_server/urls.py
+++ web_server/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('', include('news.urls'))
 ]
 
